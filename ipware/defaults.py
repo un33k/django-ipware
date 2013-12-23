d1574b47d07d5edfe167191412fedfd59fb8cfdc
Val Neekman <val@neekware.com>
2013-12-23 11:18:53 -0500

    add private B block range

--- ipware/defaults.py
+++ ipware/defaults.py
@@ -10,19 +10,25 @@ IPWARE_META_PRECEDENCE_LIST = getattr(settings,
     )
 )
 
-# List of private IP address range (lowercase)
-# (http://www.ietf.org/rfc/rfc3330.txt)
+# Private IP addresses
+# http://www.ietf.org/rfc/rfc3330.txt (IPv4)
+# http://www.ietf.org/rfc/rfc5156.txt (IPv6)
 IPWARE_PRIVATE_IP_PREFIX = getattr(settings,
     'IPWARE_PRIVATE_IP_PREFIX', (
         '0.', # non-routable or local broadcast
         '10.', # class A private block
-        '169.254.', # IPv4 link-local
-        '172.16.', '172.31.', # class B private blocks
-        '192.0.2.', # examples in documentation
+        '169.254.', # link-local block
+        '172.16.', '172.17.', '172.18.', '172.19.',
+        '172.20.', '172.21.', '172.22.', '172.23.',
+        '172.24.', '172.25.', '172.26.', '172.27.',
+        '172.28.', '172.29.', '172.30.', '172.31.', # class B private blocks
+        '192.0.2.', # reserved for documentation and example code
         '192.168.', # class C private block
         '255.', # IPv4 broadcast address
-    ) + (
+    ) + (  # the following addresses MUST be in lowercase)
+        '2001:db8:', # reserved for documentation and example code
         'fc00:' # IPv6 private block
+        'fe80:', # link-local unicast
         'ff00:', # IPv6 multicast
     )
 )
