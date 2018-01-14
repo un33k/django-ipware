40c3f8730437602cddf8aff6d2fc26568f8d3df9
Val Neekman <val@neekware.com>
2018-01-13 23:08:48 -0500

    Version 2.0, Support for proxy count

--- ipware/apps.py
+++ ipware/apps.py
@@ -1,8 +1,12 @@
+<<<<<<< HEAD
 from django.apps import AppConfig as DjangoAppConfig
+=======
+from django.apps import AppConfig
+>>>>>>> integration
 from django.utils.translation import ugettext_lazy as _
 
 
-class AppConfig(DjangoAppConfig):
+class IPwareConfig(AppConfig):
     """
     Configuration entry point for the ipware app
     """
