704bd6e50920f7b53d9ce6aa7b071365c1bef32f
Awais Qureshi <awais.qureshi@arbisoft.com>
2021-07-14 14:59:43 +0500

    Adding github actions. Remove python-version 3.5 testing matrix. Removed travis.yml.

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ classifiers = [
     'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
+    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
