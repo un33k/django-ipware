4b0ea01fd1124cd570c4cdff85284cb83a3bdd8a
Val Neekman <val@neekware.com>
2021-07-29 12:57:57 -0400

    pckMgr

--- ipware/__version__.py
+++ ipware/__version__.py
@@ -1,3 +1,8 @@
-__author__ = 'Val Neekman @ Neekware Inc. [@vneekman]'
+__title__ = 'django-ipware'
+__author__ = 'Val Neekman'
+__author_email__ = 'info@neekware.com'
 __description__ = "A Django application to retrieve user's IP address"
-__version__ = '3.0.4'
+__url__ = 'https://github.com/un33k/django-ipware'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020 Val Neekman @ Neekware Inc.'
+__version__ = '3.0.5'
