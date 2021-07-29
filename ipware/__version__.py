1dfc7177197ceddef71966f5afd09236d248adca
Val Neekman <val@neekware.com>
2021-07-29 12:49:19 -0400

    pypi rework

---
+++ ipware/__version__.py
@@ -0,0 +1,3 @@
+__author__ = 'Val Neekman @ Neekware Inc. [@vneekman]'
+__description__ = "A Django application to retrieve user's IP address"
+__version__ = '3.0.4'
--- setup.py
+++ setup.py
@@ -1,23 +1,81 @@
+#!/usr/bin/env python
+# Learn more: https://github.com/kennethreitz/setup.py
 import os
 import sys
-import setuptools
-import re
+
+from codecs import open
+from shutil import rmtree
+from setuptools import setup
+
+package = 'ipware'
+python_requires = ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*"
+here = os.path.abspath(os.path.dirname(__file__))
+packages = [package]
+requires = []
+test_requirements = []
+
+about = {}
+with open(os.path.join(here, package, '__version__.py'), 'r', 'utf-8') as f:
+    exec(f.read(), about)
+
+with open('README.md', 'r', 'utf-8') as f:
+    readme = f.read()
 
 
-def get_version(package):
-    """
-    Return package version as listed in `__version__` in `init.py`.
-    """
-    init_py = open(os.path.join(package, '__init__.py')).read()
-    return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
+def status(s):
+    print('\033[1m{0}\033[0m'.format(s))
 
 
+# 'setup.py publish' shortcut.
 if sys.argv[-1] == 'publish':
-    os.system("python setup.py build && twine upload dist/*")
-    args = {'version': get_version()}
-    print("You probably want to also tag the version now:")
-    print("  git tag -a %(version)s -m 'version %(version)s' && git push --tags" % args)
-    sys.exit()
+    try:
+        status('Removing previous builds…')
+        rmtree(os.path.join(here, 'dist'))
+    except OSError:
+        pass
+
+    status('Building Source and Wheel (universal) distribution…')
+    os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+    status('Uploading the package to PyPI via Twine…')
+    os.system('twine upload dist/*')
+
+    status('Pushing git tags…')
+    os.system('git tag v{0}'.format(about['__version__']))
+    os.system('git push --tags')
 
 
-setuptools.setup()
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__description__'],
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    packages=packages,
+    package_data={'': ['LICENSE']},
+    package_dir={'ipware': 'ipware'},
+    include_package_data=True,
+    python_requires=python_requires,
+    install_requires=requires,
+    license=about['__license__'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    cmdclass={},
+    tests_require=test_requirements,
+    extras_require={},
+    project_urls={},
+)
