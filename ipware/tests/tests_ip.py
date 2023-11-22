ad8b2f7f414199451ca466f65b6a61f23abecf31
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2023-11-22 18:14:05 -0500

    add more tests

--- ipware/tests/tests_ip.py
+++ ipware/tests/tests_ip.py
@@ -18,3 +18,27 @@ class IpTestCase(TestCase):
         }
         result = get_client_ip(request)
         self.assertEqual(result, ("177.139.233.139", True))
+
+    def test_meta_proxy_order_left_most(self):
+        request = HttpRequest()
+        request.META = {
+            'HTTP_X_FORWARDED_FOR': '177.139.233.139, 198.84.193.157, 198.84.193.158',
+        }
+        result = get_client_ip(request, proxy_order='left-most')
+        self.assertEqual(result, ("177.139.233.139", True))
+
+    def test_meta_singleton_proxy_count(self):
+        request = HttpRequest()
+        request.META = {
+            'HTTP_X_FORWARDED_FOR': '177.139.233.139, 198.84.193.157, 198.84.193.158',
+        }
+        result = get_client_ip(request, proxy_count=2)
+        self.assertEqual(result, ("177.139.233.139", True))
+        
+    def test_meta_proxy_trusted_ips_exact_ip_check(self):
+        request = HttpRequest()
+        request.META = {
+            'HTTP_X_FORWARDED_FOR': '177.139.233.139, 198.84.193.157, 198.84.193.158',
+        }
+        result = get_client_ip(request, proxy_trusted_ips=['198.84.193.157', '198.84.193.158'])
+        self.assertEqual(result, ("177.139.233.139", True))
